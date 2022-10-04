3d9f4a756bfd88f863bcd422ebdccc2086229db1
Tomas Garcia <tomasotano25@gmail.com>
2022-10-03 20:53:34 -0400

    Clase 07 spread operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -15,3 +15,22 @@ console.log(fruit)
 let user = { username: 'Tomas', age: 22 }
 let { username, age } = user
 console.log(username, user.age)
+
+// spread operator (operador de propagacion)
+
+let person = { username: 'Tomas', age: 22 }
+let country = 'MX'
+
+let data = { id: 1, ...person, country }
+console.log(data)
+
+// rest (rest parameters)
+
+function sum(num, ...values) {
+  console.log(values)
+  console.log(num + values[0])
+  return num + values[0];
+}
+
+sum(1, 1, 2, 3)
+
