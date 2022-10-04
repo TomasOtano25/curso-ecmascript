5c0bb04994c7a7b5eb0211a67fadc8c2abe5da3d
Tomas Garcia <tomasotano25@gmail.com>
2022-10-04 16:42:48 -0400

    Clase 17 string padding

---
+++ src/es8/02-string-padding.js
@@ -0,0 +1,8 @@
+const string = 'Hello'
+
+console.log(string.padStart(10, 'hi')) // crea una cadena de texto de 10 elementos y rellena con el termino colocado aqui
+
+console.log(string.padStart(10, '_'))
+
+console.log(string.padStart(6, '_'))
+console.log(string.padEnd(6, '_'))
