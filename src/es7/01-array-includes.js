c6c79ea42c7ecbead2065c82fdaa9023f300b27b
Tomas Garcia <tomasotano25@gmail.com>
2022-10-04 16:31:57 -0400

    Clase 16 object entries y object values

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,8 @@
+let number = [1, 3, 4, 6, 7, 8]
+
+console.log(number.includes(4))
+console.log(number.includes(9))
+
+const list = ['Oscar', 'Tomas', 'Aavid']
+
+console.log(list.includes('Oscar'))
