86473d6b25e04e78c7cb351337db4e1d677230aa
Tomas Garcia <tomasotano25@gmail.com>
2022-10-03 21:12:36 -0400

    Clase 10 promises

---
+++ src/es6/06-promises.js
@@ -0,0 +1,16 @@
+// promises (algo que va pasar)
+
+const anotherFunction = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve('Hey!!')
+    }
+    else {
+      reject('Whoooooops!')
+    }
+  })
+}
+
+anotherFunction()
+  .then((response) => console.log(response))
+  .catch(error => console.log(error))
