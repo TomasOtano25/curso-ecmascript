e4ae6907496831741c62d2e65db04899b828fc94
Tomas Garcia <tomasotano25@gmail.com>
2022-10-05 20:48:02 -0400

    Clase 23 optional chaining

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,13 @@
+const users = {
+  tomas: {
+    country: 'RD'
+  },
+  ana: {
+    country: 'CO'
+  }
+}
+
+console.log(users?.developer?.country)
+// console.log(users.tomas.country)
+// console.log(users.tomas.age)
+// console.log(users.developer.country)
