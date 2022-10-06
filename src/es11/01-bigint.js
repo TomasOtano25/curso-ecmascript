b9a761c5db20509a5e62057a2e62bd4b9aebf63c
Tomas Garcia <tomasotano25@gmail.com>
2022-10-05 20:55:31 -0400

    Clase 24 bigint y nullish

---
+++ src/es11/01-bigint.js
@@ -0,0 +1,5 @@
+const aBigNumber = 7989674546798642317568n;
+const anotherBigNumber = BigInt(7989674546798642317568); // Volumenes altos de numeros
+
+console.log(aBigNumber)
+console.log(anotherBigNumber)
---
+++ src/es11/02-nullish.js
@@ -0,0 +1,8 @@
+// valor por defecto cuando es nulo
+
+// const anotherNumber = 1;
+const anotherNumber = null;
+
+const validate = anotherNumber ?? 5;
+
+console.log(validate)
